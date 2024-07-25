e9c71e2d79663eaa8b35520a42f6e9b948438aca
Ricardo M. <lordrip@gmail.com>
2024-07-25 07:49:53 -0400

    Temp: first commit

--- packages/ui/src/global.d.ts
+++ packages/ui/src/global.d.ts
@@ -4,3 +4,4 @@ declare let NODE_ENV: string;
 declare let __GIT_HASH: string;
 declare let __GIT_DATE: string;
 declare let __KAOTO_VERSION: string;
+declare module 'clippyjs';
--- packages/ui/src/main.tsx
+++ packages/ui/src/main.tsx
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import { FilterDOMPropsKeys, filterDOMProps } from 'uniforms';
+import clippy from 'clippyjs';
+import './assets/clippy.css';
+
+clippy.load(
+  'Clippy',
+  (agent: any) => {
+    // do anything with the loaded agent
+    agent.show();
+
+    setInterval(() => {
+      agent.animate();
+    }, 10_000);
+  },
+  undefined,
+  './src/assets/agents/',
+);
 
 filterDOMProps.register(
   'inputRef' as FilterDOMPropsKeys,
