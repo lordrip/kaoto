31cffd637074a5d3a6b0faa4304149bef13416e2
Ricardo M. <lordrip@gmail.com>
2024-10-21 13:16:21 +0200

    Temp: Extract context menu logic to hooks

--- packages/ui/src/components/Visualization/Custom/ContextMenu/ItemDeleteStep.tsx
+++ packages/ui/src/components/Visualization/Custom/ContextMenu/ItemDeleteStep.tsx
@@ -1,13 +1,9 @@
 import { TrashIcon } from '@patternfly/react-icons';
 import { ContextMenuItem } from '@patternfly/react-topology';
-import { FunctionComponent, PropsWithChildren, useCallback, useContext } from 'react';
+import { FunctionComponent, PropsWithChildren } from 'react';
 import { IDataTestID } from '../../../../models';
 import { IVisualizationNode } from '../../../../models/visualization/base-visual-entity';
-import { EntitiesContext } from '../../../../providers/entities.provider';
-import {
-  ACTION_ID_CONFIRM,
-  ActionConfirmationModalContext,
-} from '../../../../providers/action-confirmation-modal.provider';
+import { useDeleteStep } from '../hooks/delete-step.hook';
 
 interface ItemDeleteStepProps extends PropsWithChildren<IDataTestID> {
   vizNode: IVisualizationNode;
@@ -15,24 +11,7 @@ interface ItemDeleteStepProps extends PropsWithChildren<IDataTestID> {
 }
 
 export const ItemDeleteStep: FunctionComponent<ItemDeleteStepProps> = (props) => {
-  const entitiesContext = useContext(EntitiesContext);
-  const deleteModalContext = useContext(ActionConfirmationModalContext);
-
-  const onRemoveNode = useCallback(async () => {
-    if (props.loadActionConfirmationModal) {
-      /** Open delete confirm modal, get the confirmation  */
-      const isDeleteConfirmed = await deleteModalContext?.actionConfirmation({
-        title: 'Permanently delete step?',
-        text: 'Step and its children will be lost.',
-      });
-
-      if (isDeleteConfirmed !== ACTION_ID_CONFIRM) return;
-    }
-
-    props.vizNode?.removeChild();
-    entitiesContext?.updateEntitiesFromCamelResource();
-  }, [deleteModalContext, entitiesContext, props.loadActionConfirmationModal, props.vizNode]);
-
+  const { onRemoveNode } = useDeleteStep(props.vizNode);
   return (
     <ContextMenuItem onClick={onRemoveNode} data-testid={props['data-testid']}>
       <TrashIcon /> Delete
